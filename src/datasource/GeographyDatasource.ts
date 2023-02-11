35b8cb6eb04aa550908873f50899c2adc7a24d8f
john_Gamboa <jgamboa65@uan.edu.co>
2023-02-10 22:56:05 -0500

    I recuperated datafile because i dont make merge

---
+++ src/datasource/GeographyDatasource.ts
@@ -0,0 +1,66 @@
+import { MessageError } from '../utilities/DebugUtilities';
+import { QueryTypes } from 'sequelize';
+import debugLib from 'debug';
+import { executeSQL } from '../database/database';
+
+const debug = debugLib('tc:geographyDataSource');
+
+export default class GeographyDataSource 
+{
+
+    public static readonly getRangeByUser = async function (idBuyer:number): Promise<[]> {
+        debug('Starts the database query of the search range by user');
+        try {
+            const result = await executeSQL(
+                `SELECT com_rango_busqueda as rango
+                FROM tr_data_base.comprador 
+                WHERE com_id=$idBuyer;`,
+                QueryTypes.SELECT,
+                {idBuyer}
+            );
+            if (result.length > 0) {
+                return Promise.resolve(result);
+            } else {
+                debug(`${MessageError}`, '404 TR_DATA_BASE');
+                const bodyErrorSearchConfigInfo = {
+                    CodeError: 'SELECT-RANGE-BY-USER-404-DB',
+                    Reason: 'BD error TR_DATA_BASE',
+                    StatusCode: '404',
+                };
+                return Promise.reject(bodyErrorSearchConfigInfo);
+            }
+
+        } catch (err) {
+            debug(`[%s] ${MessageError}`, err);
+            return Promise.reject({ Code: 'SELECT-RANGE-BY-USER', Reason: err });
+        }
+    }
+
+    public static readonly getOverwriteRange = async (): Promise<[]> => {
+        debug('Starts the database query of the search products types');
+        try {
+            const result = await executeSQL(. //El query esta mal, select solo es para traer datos, buscar como se hace un update en sql
+                `select * from tr_data_base.comprador;
+                UPDATE tr_data_base.comprador 
+                SET com_rango_busqueda=20 where com_id=2;`,
+                QueryTypes.SELECT,  // reemplazar la palabra select por UPDATE
+                {}
+                );
+            if (result.length > 0) {
+                return Promise.resolve(result);
+            } else {
+                debug(`${MessageError}`, '404 TR_DATA_BASE');
+                const bodyError = {
+                    CodeError: 'SELECT-SEARCH-PRODUCT-TYPES-404-DB',
+                    Reason: 'BD error TR_DATA_BASE',
+                    StatusCode: '404',
+                };
+                return Promise.reject(bodyError);
+            }
+
+        } catch (err) {
+            debug(`[%s] ${MessageError}`, err);
+            return Promise.reject({ Code: 'SELECT-SEARCH-PRODUCT-TYPES', Reason: err });
+        }
+    }
+}
---
+++ src/services/ServiceController.ts
@@ -0,0 +1,30 @@
+import GeographyDataSource from '../datasource/GeographyDatasource';
+import debugLib from 'debug';
+import { response } from 'express';
+const debug = debugLib('John');
+
+export class GeographyService {
+    static getGeography() {
+        throw new Error('Method not implemented.');
+    }
+
+    public static async getRangeByUser(idBuyer:number): Promise<any> {
+     try {        
+      const response =  await GeographyDataSource.getRangeByUser(idBuyer);      
+      return Promise.resolve(response);
+     } catch (err) {
+        debug('Error trying to obtain motorcycle brands %s ', err);
+        return Promise.reject(err);
+     }
+    }
+
+    public static async getOverwriteRange(): Promise<[]> {
+        try {
+           const response =  await GeographyDataSource.getOverwriteRange(); //debe enviar el id del usuario al que se quiere modificar el valor y el nuevo valor 
+           return Promise.resolve(response);
+        } catch (err) {
+           debug('Error trying to obtain products types- %s ', err);
+           return Promise.reject(err);
+        }
+       }
+}
